c6727ad95364aaf4469e096203069c19325cd682
Martin <martiniglesias21@gmail.com>
2022-08-18 16:11:51 -0300

    iniciando repositorio react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let i = 0
+  i++
+  console.log(i)
   return (
     <div className="App">
       <header className="App-header">
